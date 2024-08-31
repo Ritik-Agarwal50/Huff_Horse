361f999592c63fb95f13e111f034d3a26e6f4fb5
Ritik <ritikagarwal570@gmail.com>
2024-08-31 13:36:14 +0530

    assembly

--- src/horseStoreV2/horseStore.sol
+++ src/horseStoreV2/horseStore.sol
@@ -5,12 +5,14 @@ contract horseStore{
     uint256 public horseNumber;
     function updateHorseNumber(uint256 _horseNumber) external {
         assembly {
-            
+            sstore(horseNumber.slot, _horseNumber)
         }
     }
     function readNumberFromHorseStore() external view returns(uint256){
         assembly{
-            
+            let num := sload(horseNumber.slot)
+            mstore(0,num)
+            return (0,0x20)
         }
     }
 }
