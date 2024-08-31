561512e8085641f73eca9630e07a21e1a0eccf1f
Ritik <ritikagarwal570@gmail.com>
2024-08-31 13:31:30 +0530

    assembly

---
+++ src/horseStoreV2/horseStore.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract horseStore{
+    uint256 public horseNumber;
+    function updateHorseNumber(uint256 _horseNumber) external {
+        assembly {
+            
+        }
+    }
+    function readNumberFromHorseStore() external view returns(uint256){
+        assembly{
+            
+        }
+    }
+}
